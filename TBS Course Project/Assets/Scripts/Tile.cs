269d17312c27b3e8870b1fb75b12797333f62982
Unknown <joohyung122002@gmail.com>
2019-12-20 01:59:00 -0800

    Created Art Assets + bug fixes
    nani

--- TBS Course Project/Assets/Scripts/Tile.cs
+++ TBS Course Project/Assets/Scripts/Tile.cs
@@ -7,7 +7,7 @@ public class Tile : MonoBehaviour
 
     private SpriteRenderer rend;
     public GameObject cursor;
-    public enum TerrainType { None, Plains, Forest, Mountains, Sand, Water }
+    public enum TerrainType { None, Plains, Forest, Mountain, Sand, Water }
     public TerrainType terrainType;
     public bool randomGeneration;
     public Sprite[] tileGraphics;
@@ -26,10 +26,48 @@ public class Tile : MonoBehaviour
         {
             int randTile = Random.Range(0, tileGraphics.Length);
             rend.sprite = tileGraphics[randTile];
+            switch (randTile)
+            {
+                case 0:
+                    terrainType = TerrainType.Mountain;
+                    break;
+                case 1:
+                    terrainType = TerrainType.Forest;
+                    break;
+                case 2:
+                    terrainType = TerrainType.Plains;
+                    break;
+                case 3:
+                    terrainType = TerrainType.Water;
+                    break;
+                case 4:
+                    terrainType = TerrainType.Sand;
+                    break;
+            }
         }
         else if (terrainType == TerrainType.None)
         {
             rend.sprite = null;
+        }
+        else if (terrainType == TerrainType.Plains)
+        {
+            rend.sprite = tileGraphics[2];
+        }
+        else if (terrainType == TerrainType.Forest)
+        {
+            rend.sprite = tileGraphics[1];
+        }
+        else if (terrainType == TerrainType.Mountain)
+        {
+            rend.sprite = tileGraphics[0];
+        }
+        else if (terrainType == TerrainType.Sand)
+        {
+            rend.sprite = tileGraphics[4];
+        }
+        else if (terrainType == TerrainType.Water)
+        {
+            rend.sprite = tileGraphics[3];
         }
 
         gm = FindObjectOfType<GameMaster>();
@@ -52,15 +90,15 @@ public class Tile : MonoBehaviour
             return false;
         }
 
-        if (unitType == Unit.UnitType.Axe && terrainType == TerrainType.Mountains)
+        if (unitType == Unit.UnitType.Axe && (terrainType == TerrainType.None && randomGeneration == false || terrainType == TerrainType.Mountain))
         {
             return false;
         }
-        else if (unitType == Unit.UnitType.Sword && terrainType == TerrainType.Mountains)
+        else if (unitType == Unit.UnitType.Sword && (terrainType == TerrainType.None && randomGeneration == false || terrainType == TerrainType.Mountain))
         {
             return false;
         }
-        else if (unitType == Unit.UnitType.Bow && terrainType == TerrainType.Mountains)
+        else if (unitType == Unit.UnitType.Bow && (terrainType == TerrainType.None && randomGeneration == false || terrainType == TerrainType.Mountain))
         {
             return false;
         }
